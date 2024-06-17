194de3b2928346e80e343f308a40ac4939ee6c88
Drex <nicknamedrex@gmail.com>
2024-06-17 19:12:01 +0200

    Add icon

--- src/main/java/me/drex/ppwt/PerPlayerWanderingTraders.java
+++ src/main/java/me/drex/ppwt/PerPlayerWanderingTraders.java
@@ -2,13 +2,8 @@ package me.drex.ppwt;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.server.level.ServerPlayer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PerPlayerWanderingTraders implements ModInitializer {
-
-
-    public static final Logger LOGGER = LoggerFactory.getLogger("perplayerwanderingtraders");
 
     public static final ThreadLocal<ServerPlayer> PLAYER_ARG = new ThreadLocal<>();
 
